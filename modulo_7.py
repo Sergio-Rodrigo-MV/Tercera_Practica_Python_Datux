c3d9dc965a6e656a0bffb50c31d0c5eab0fae799
Sergio-Rodrigo-MV <142172078+Sergio-Rodrigo-MV@users.noreply.github.com>
2023-08-18 20:23:20 -0500

    Update modulo_7.py

--- modulo_7.py
+++ modulo_7.py
@@ -1,18 +1,19 @@
-# Importar el módulo random
+#Importamos el módulo random
 import random
 
-# Definir una función que genere 20 números enteros aleatorios entre 0 y 100 y devuelva una lista
+#Defino una función que genere 20 números enteros aleatorios entre 0 y 100 y devuelva una lista
+#usando el método random.
 def generar_lista():
     lista = []
     for i in range(20):
         lista.append(random.randint(0, 100))
     return lista
 
-# Definir una función que muestre la lista obtenida por pantalla
+#Defino una función que muestre la lista obtenida por pantalla.
 def mostrar_lista(lista):
     print(lista)
 
-# Definir una función que ordene los valores de la lista y la muestre por pantalla
+#Defino una función que ordene los valores de la lista y la muestre por pantalla.
 def ordenar_lista(lista):
     lista.sort()
     print(lista)
